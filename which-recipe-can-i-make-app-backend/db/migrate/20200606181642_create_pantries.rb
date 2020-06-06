620fa40e6c62035335b736ed7a49b3f4b46834be
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-06 13:19:10 -0500

    generate models and migrations

---
+++ which-recipe-can-i-make-app-backend/db/migrate/20200606181642_create_pantries.rb
@@ -0,0 +1,11 @@
+class CreatePantries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pantries do |t|
+      t.string :name
+      t.string :quantity
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
