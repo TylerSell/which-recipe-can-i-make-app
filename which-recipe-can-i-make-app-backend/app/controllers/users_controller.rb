7b6c72760360faf2e8a750fc5ea9200330b0b2ec
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-06 14:56:50 -0500

    build user controller actions

--- which-recipe-can-i-make-app-backend/app/controllers/users_controller.rb
+++ which-recipe-can-i-make-app-backend/app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+    def create 
+        user = User.create(user_params)
+
+        if user.save 
+            session[:user_id] = user.id 
+            render json: user 
+        else 
+            render json: {message: 'Something went wrong please try again'}
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user 
+    end
+
+    def update 
+        user = User.find_by(id: session[:user_id])
+        user.update(user_params)
+        if user.save 
+            render json: user 
+        else 
+            render json: {message: 'Something went wrong and your changes were not saved. Please try again.'}
+        end
+    end
+
+
+# --------------------------------------------------------------
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end 
+
 end
