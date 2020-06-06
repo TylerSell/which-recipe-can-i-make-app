c9bf06f8f703e3331d9d1384496923f58668db7b
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-06 14:10:25 -0500

    run migrations and set up model associations

--- which-recipe-can-i-make-app-backend/app/models/recipe.rb
+++ which-recipe-can-i-make-app-backend/app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :ingredients
+  has_many :instructions
 end
--- which-recipe-can-i-make-app-backend/app/models/user.rb
+++ which-recipe-can-i-make-app-backend/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :recipes
+    has_many :pantry_items
 end
