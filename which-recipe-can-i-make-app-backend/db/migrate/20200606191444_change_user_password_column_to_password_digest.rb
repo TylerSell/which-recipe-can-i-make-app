de59429220a19a6439b3baeffdbf551af2f66cfc
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-06 14:16:54 -0500

    rename password to password_digest for has_secure_password

---
+++ which-recipe-can-i-make-app-backend/db/migrate/20200606191444_change_user_password_column_to_password_digest.rb
@@ -0,0 +1,5 @@
+class ChangeUserPasswordColumnToPasswordDigest < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- which-recipe-can-i-make-app-backend/db/schema.rb
+++ which-recipe-can-i-make-app-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_190412) do
+ActiveRecord::Schema.define(version: 2020_06_06_191444) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_190412) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
