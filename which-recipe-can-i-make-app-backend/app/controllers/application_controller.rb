5b33d9ee0f991686d6b14dfb35afe7fc37065eb3
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-06 14:39:16 -0500

    build application controller and stub for not logged in error message

--- which-recipe-can-i-make-app-backend/app/controllers/application_controller.rb
+++ which-recipe-can-i-make-app-backend/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+    protect_from_forgery with: :exception
+    # before_action :message_if_not_authenticated 
+    helper_method :current_user
+
+    # ---------------------------------------------------------
+
+    private 
+
+    def current_user 
+        @user ||= User.find_by(id: session[:user_id])
+    end 
+
+    def user_authenticated 
+        !!session[:user_id]
+    end 
+
+    # def message_if_not_authenticated
+    #     self.errors.add(:not_logged_in, "You are not logged in or do not have privelidges to view this.") unless user_authenticated
+    # end
+
 end
