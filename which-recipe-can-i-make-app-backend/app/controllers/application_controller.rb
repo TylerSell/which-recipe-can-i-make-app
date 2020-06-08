6bdb9015a143d4c83831f3d8a7298e0ebaf5639c
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-07 23:35:52 -0500

    updating fetch request and config files

--- which-recipe-can-i-make-app-backend/app/controllers/application_controller.rb
+++ which-recipe-can-i-make-app-backend/app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::API
 
     private 
 
-    def current_user 
-        @user ||= User.find_by(id: session[:user_id])
-    end 
+    # def current_user 
+    #     @user ||= User.find_by(id: session[:user_id])
+    # end 
 
-    def user_authenticated 
-        !!session[:user_id]
-    end 
+    # def user_authenticated 
+    #     !!session[:user_id]
+    # end 
 
     # def message_if_not_authenticated
     #     self.errors.add(:not_logged_in, "You are not logged in or do not have privelidges to view this.") unless user_authenticated
--- which-recipe-can-i-make-app-frontend/src/index.js
+++ which-recipe-can-i-make-app-frontend/src/index.js
@@ -3,14 +3,10 @@ const USERS_URL = `${BASE_URL}/users`
 const RECIPES_URL = `${BASE_URL}/recipes`
 const mainSection = document.querySelector('main')
 
-document.addEventListener("DOMContentLoaded", getUser())
+document.addEventListener("DOMContentLoaded", () => getUser())
 
 const getUser = () => {
     fetch(USERS_URL)
     .then(resp => resp.json())
-    .then(json => {
-        if (json.message) {
-            alert(json.message)
-        }
-    })
+    .then(json => console.log(json))
 }
