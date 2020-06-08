27a9eab05312f895345b18aff5391ae09bc97cce
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-08 09:36:40 -0500

    add session controller

--- which-recipe-can-i-make-app-backend/config/routes.rb
+++ which-recipe-can-i-make-app-backend/config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :instructions
-  resources :ingredients
-  resources :recipes
-  resources :pantry_items
-  resources :users
+  
+  
+  
+  resources :users do 
+    resources :recipes
+    resources :pantry_items
+    resources :instructions
+    resources :ingredients
+  end
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
