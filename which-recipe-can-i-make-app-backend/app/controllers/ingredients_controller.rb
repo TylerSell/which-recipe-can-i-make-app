6288257e55d5fb3cb39c82a10a5bd861cee110ba
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-08 13:39:17 -0500

    build instruction and ingredient controllers and test

--- which-recipe-can-i-make-app-backend/app/controllers/ingredients_controller.rb
+++ which-recipe-can-i-make-app-backend/app/controllers/ingredients_controller.rb
@@ -1,10 +1,23 @@
 class IngredientsController < ApplicationController
+
+    def index 
+        recipe = Recipe.find_by(id: params[:recipe_id])
+        ingredients = recipe.ingredients
+        render json: ingredients
+    end
     
     def create 
-        recipe = Recipe.find_by(id: :recipe_id)
+        recipe = Recipe.find_by(id: params[:recipe_id])
         ingredient = recipe.ingredient.build(ingredient_params)
 
         render json: ingredient.save ? ingredient : {message: 'Something went wrong please try again'}
+    end
+
+    def update 
+        ingredient = Ingredient.find_by(id: params[:id])
+        ingredient.update(ingredient_params)
+
+        render json: ingredient.save ? ingredient : {message: 'Something went wrong and your changes were not saved. Please try again.'}
     end
 
     # ---------------------------------------------------------------------
--- which-recipe-can-i-make-app-backend/app/controllers/instructions_controller.rb
+++ which-recipe-can-i-make-app-backend/app/controllers/instructions_controller.rb
@@ -1,11 +1,25 @@
 class InstructionsController < ApplicationController
+
+    def index 
+        recipe = Recipe.find_by(id: params[:recipe_id])
+        instructions = recipe.instructions
+        render json: instructions
+    end
     
     def create 
-        recipe = Recipe.find_by(id: :recipe_id)
+        recipe = Recipe.find_by(id: params[:recipe_id])
         instruction = recipe.instruction.build(instruction_params)
 
         render json: instruction.save ? instruction : {message: 'Something went wrong please try again'}
     end
+
+    def update 
+        instruction = Instruction.find_by(id: params[:id])
+        instruction.update(instruction_params)
+
+        render json: instruction.save ? instruction : {message: 'Something went wrong and your changes were not saved. Please try again.'}
+    end
+
 
     # ---------------------------------------------------------------------
 
