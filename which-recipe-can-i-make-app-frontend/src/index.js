ce0722c8b41a1182f472f1197778a504e45f05bf
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-06-13 13:30:49 -0500

    working on showing and hiding select forms

--- which-recipe-can-i-make-app-frontend/src/index.js
+++ which-recipe-can-i-make-app-frontend/src/index.js
@@ -17,6 +17,9 @@ const loadPage = () => {
         }
     })
     .catch(console.log)
+
+    userLoginForm();
+    userSignupForm();
 }
 
 const homePage = () => {
@@ -31,14 +34,14 @@ const homePage = () => {
     signUpButton.setAttribute("id", "signUpButton")
     signUpButton.setAttribute("class", "btn btn-outline-info mr-2")
     signUpButton.innerHTML = "Sign Up"
-    signUpButton.addEventListener("click", userSignupForm)
+    signUpButton.addEventListener("click", displaySignupForm)
     headerForm.appendChild(signUpButton)
 
     const logInButton = document.createElement('button')
     logInButton.setAttribute("id", "logInButton")
     logInButton.setAttribute("class", "btn btn-success")
     logInButton.innerHTML = 'Login'
-    signUpButton.addEventListener("click", userLoginForm)
+    signUpButton.addEventListener("click", displayLoginForm)
     headerForm.appendChild(logInButton)
     
     pageHeader.appendChild(span)
@@ -109,115 +112,20 @@ const displayRecipes = (event) => {
     event.preventDefault();
 }
 
-const userSignupForm = (event) => {
+const displaySignupForm = (event) => {
     event.preventDefault();
 
-    const row = document.createElement('div')
-    row.setAttribute("class", "row flex-xl-nowrap justify-content-center")
-    const div = document.createElement('div')
-    div.setAttribute("class", "col-6 mx-auto")
-    const card = document.createElement('div')
-    card.setAttribute("class", "shadow card")
-    const cardHeader = document.createElement('h5')
-    cardHeader.setAttribute("class", "card-header text-center")
-    cardHeader.innerHTML = "User Login"
-    const cardBody = document.createElement('div')
-    cardBody.setAttribute("class", "card-body")
-    const cardText = document.createElement('p')
-    cardText.setAttribute("class", "card-text")
-    const form = document.createElement('form')
-    
-    const firstNameGroup = document.createElement('div')
-    firstNameGroup.setAttribute("class", "form-group")
-    const firstNameLabel = document.createElement('label')
-    firstNameLabel.setAttribute("for", "user_first_name")
-    firstNameLabel.innerHTML = "First Name"
-    const firstNameInput = document.createElement('input')
-    firstNameInput.setAttribute("class", "form-control")
-    firstNameInput.setAttribute("type", "text")
-    firstNameInput.setAttribute("name", "user[first_name]")
-    firstNameInput.setAttribute("id", "user_first_name")
-
-    const lastNameGroup = document.createElement('div')
-    lastNameGroup.setAttribute("class", "form-group")
-    const lastNameLabel = document.createElement('label')
-    lastNameLabel.setAttribute("for", "user_last_name")
-    lastNameLabel.innerHTML = "Last Name"
-    const lastNameInput = document.createElement('input')
-    lastNameInput.setAttribute("class", "form-control")
-    lastNameInput.setAttribute("type", "text")
-    lastNameInput.setAttribute("name", "user[last_name]")
-    lastNameInput.setAttribute("id", "user_last_name")
-
-    const emailGroup = document.createElement('div')
-    emailGroup.setAttribute("class", "form-group")
-    const emailLabel = document.createElement('label')
-    emailLabel.setAttribute("for", "user_email")
-    emailLabel.innerHTML = "Email"
-    const emailInput = document.createElement('input')
-    emailInput.setAttribute("class", "form-control")
-    emailInput.setAttribute("type", "text")
-    emailInput.setAttribute("name", "user[email]")
-    emailInput.setAttribute("id", "user_email")
-
-    const passwordGroup = document.createElement('div')
-    passwordGroup.setAttribute("class", "form-group")
-    const passwordLabel = document.createElement('label')
-    passwordLabel.setAttribute("for", "user_password")
-    passwordLabel.innerHTML = "Password"
-    const passwordInput = document.createElement('input')
-    passwordInput.setAttribute("class", "form-control")
-    passwordInput.setAttribute("type", "password")
-    passwordInput.setAttribute("name", "user[password]")
-    passwordInput.setAttribute("id", "user_password")
-
-    const submitButton = document.createElement('input')
-    submitButton.setAttribute("class", "btn btn-primary btn-block text-decoration-none")
-    submitButton.setAttribute("type", "submit")
-    submitButton.setAttribute("name", "commit")
-    submitButton.setAttribute("value", "Login")
-    submitButton.setAttribute("data-disable-with", "Login")
-
-    // build firstNameGroup then attach to form
-    firstNameGroup.appendChild(firstNameLabel)
-    firstNameGroup.appendChild(firstNameInput)
-    form.appendChild(firstNameGroup)
-    // build lastNameGroup then attach to form
-    lastNameGroup.appendChild(lastNameLabel)
-    lastNameGroup.appendChild(lastNameInput)
-    form.appendChild(lastNameGroup)
-    // build emailGroup then attach to form
-    emailGroup.appendChild(emailLabel)
-    emailGroup.appendChild(emailInput)
-    form.appendChild(emailGroup)
-    // build passwordGroup then attach to form
-    passwordGroup.appendChild(passwordLabel)
-    passwordGroup.appendChild(passwordInput)
-    form.appendChild(passwordGroup)
-    // build button then attach to form
-    form.appendChild(submitButton)
-    // attach form to cardText
-    cardText.appendChild(form)
-    // attach cardText to cardBody
-    cardBody.appendChild(cardText)
-    // attach cardHeader then cardBody to card
-    card.appendChild(cardHeader)
-    card.appendChild(cardBody)
-    // attach card to div
-    div.appendChild(card)
-    // attach div to row
-    row.appendChild(div)
-    // attach row to mainSection
-    mainSection.appendChild(row)
+    document.getElementById('signupRow').removeAttribute("style")
+    document.getElementById('loginRow').style.display = "none"
 }
 
-const userLoginForm = (event) => {
-    event.preventDefault();
-
+const userSignupForm = () => {
     const row = document.createElement('div')
     row.setAttribute("class", "row flex-xl-nowrap justify-content-center")
+    row.style.display = "none"
+    row.setAttribute("id", "signupRow")
     const div = document.createElement('div')
-    div.setAttribute("class", "col-6 mx-auto")
+    div.setAttribute("class", "mx-auto")
     const card = document.createElement('div')
     card.setAttribute("class", "shadow card w-50")
     const cardHeader = document.createElement('h5')
@@ -312,5 +220,84 @@ const userLoginForm = (event) => {
     // attach row to mainSection
     mainSection.appendChild(row)
 }
+
+const displayLoginForm = (event) => {
+    event.preventDefault();
+
+    document.getElementById('loginRow').removeAttribute("style")
+    document.getElementById('signupRow').style.display = "none"
+}
+
+const userLoginForm = () => {
+    const row = document.createElement('div')
+    row.setAttribute("class", "row flex-xl-nowrap justify-content-center")
+    row.style.display = "none"
+    row.setAttribute("id", "loginRow")
+
+    const card = document.createElement('div')
+    card.setAttribute("class", "shadow card w-50 mx-auto my-5")
+    const cardHeader = document.createElement('h5')
+    cardHeader.setAttribute("class", "card-header text-center")
+    cardHeader.innerHTML = "User Login"
+    const cardBody = document.createElement('div')
+    cardBody.setAttribute("class", "card-body")
+    const cardText = document.createElement('p')
+    cardText.setAttribute("class", "card-text")
+    const form = document.createElement('form')
+
+    const emailGroup = document.createElement('div')
+    emailGroup.setAttribute("class", "form-group")
+    const emailLabel = document.createElement('label')
+    emailLabel.setAttribute("for", "user_email")
+    emailLabel.innerHTML = "Email"
+    const emailInput = document.createElement('input')
+    emailInput.setAttribute("class", "form-control")
+    emailInput.setAttribute("type", "text")
+    emailInput.setAttribute("name", "user[email]")
+    emailInput.setAttribute("id", "user_email")
+
+    const passwordGroup = document.createElement('div')
+    passwordGroup.setAttribute("class", "form-group")
+    const passwordLabel = document.createElement('label')
+    passwordLabel.setAttribute("for", "user_password")
+    passwordLabel.innerHTML = "Password"
+    const passwordInput = document.createElement('input')
+    passwordInput.setAttribute("class", "form-control")
+    passwordInput.setAttribute("type", "password")
+    passwordInput.setAttribute("name", "user[password]")
+    passwordInput.setAttribute("id", "user_password")
+
+    const submitButton = document.createElement('input')
+    submitButton.setAttribute("class", "btn btn-primary btn-block text-decoration-none")
+    submitButton.setAttribute("type", "submit")
+    submitButton.setAttribute("name", "commit")
+    submitButton.setAttribute("value", "Login")
+    submitButton.setAttribute("data-disable-with", "Login")
+    // submitButton.setEventListener("click", getUser)
+
+    // build emailGroup then attach to form
+    emailGroup.appendChild(emailLabel)
+    emailGroup.appendChild(emailInput)
+    form.appendChild(emailGroup)
+    // build passwordGroup then attach to form
+    passwordGroup.appendChild(passwordLabel)
+    passwordGroup.appendChild(passwordInput)
+    form.appendChild(passwordGroup)
+    // build button then attach to form
+    form.appendChild(submitButton)
+    // attach form to cardText
+    cardText.appendChild(form)
+    // attach cardText to cardBody
+    cardBody.appendChild(cardText)
+    // attach cardHeader then cardBody to card
+    card.appendChild(cardHeader)
+    card.appendChild(cardBody)
+    // attach card to row
+    row.appendChild(card)
+    // attach row to mainSection
+    mainSection.appendChild(row)
+}
+
+
 
 
